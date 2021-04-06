0150a3b60f3c34ab5c0d5fbccfcbacb1c5a08bd6
KenNaNa <m13425477079@163.com>
2021-04-06 09:37:49 +0800

    Update main.ts

--- vue3-ts-vite2/src/main.ts
+++ vue3-ts-vite2/src/main.ts
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router/index'
+import store from './store/index'
+import 'vant/lib/index.css'
+import vant from 'vant'
 
-createApp(App).mount('#app')
+import { Toast } from 'vant'
+import "./utils/rem"
+import './api/mock'
+import Banner from "./components/Banner/index.vue"
+const app = createApp(App)
+app.component('banner', Banner).use(router).use(store).use(vant).mount('#app')
+app.config.globalProperties = {
+    "$toast": Toast,
+}
