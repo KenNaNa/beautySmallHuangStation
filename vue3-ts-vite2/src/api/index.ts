2af92191dd5446316727e70bad3ac2f140f23b3e
KenNaNa <m13425477079@163.com>
2021-04-07 09:30:17 +0800

    Update and rename index.txt to index.ts

---
+++ vue3-ts-vite2/src/api/index.ts
@@ -0,0 +1,35 @@
+import type { AxiosPromise } from 'axios'
+import request from './request'
+
+// 获取首页banner新闻数据
+export const getBannerList = (): AxiosPromise => {
+    return request({
+        url: '/bannerList'
+    })
+}
+
+//  获取首页newsList数据
+export const getNewsList = (): AxiosPromise => {
+    return request({
+        url: '/newsList'
+    })
+}
+
+//获取newsDetail数据
+export const getNewsDetail = (id: any): AxiosPromise => {
+    return request.post('/detailList', {
+        id
+    })
+}
+
+// 登录验证
+export const toLogin = (data: Object): AxiosPromise => {
+    return request.post('/login', data)
+}
+
+export default {
+    getBannerList,
+    getNewsList,
+    getNewsDetail,
+    toLogin
+}
