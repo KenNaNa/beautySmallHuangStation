53a2cec3e506870acb9fd678c89c96d7394ec41c
KenNaNa <m13425477079@163.com>
2021-04-08 15:52:35 +0800

    Update app.ts

--- taro-ts-vue3/src/app.ts
+++ taro-ts-vue3/src/app.ts
@@ -1,9 +1,18 @@
 import { createApp } from 'vue'
 import './app.scss'
-
+import "vant/lib/index.css";
+import vant from 'vant'
+import { Toast } from 'vant'
+import Banner from './components/Banner/index.vue'
+import './api/mock'
+import Taro from '@tarojs/taro'
 const App = createApp({
-  onShow (options) {},
+  onShow(options) { },
   // 入口组件不需要实现 render 方法，即使实现了也会被 taro 所覆盖
 })
-
+App.use(vant).component('banner', Banner)
+App.config.globalProperties = {
+  "$toast": Toast,
+  "$taro": Taro
+}
 export default App
