e0dc67448993d68c064a3bccdeaf4e81b998cdb8
KenNaNa <m13425477079@163.com>
2021-04-08 15:50:31 +0800

    Update global.d.ts

--- taro-ts-vue3/global.d.ts
+++ taro-ts-vue3/global.d.ts
@@ -15,7 +15,7 @@ declare namespace NodeJS {
   }
 }
 
-declare module ".vue" {
+declare module "*.vue" {
   import { DefineComponent } from 'vue'
   const component: DefineComponent<{}, {}, any>
   export default component
