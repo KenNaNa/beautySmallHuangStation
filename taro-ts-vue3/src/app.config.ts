619840561a58bbc1bed9002c610200a9970f13e5
KenNaNa <m13425477079@163.com>
2021-04-09 12:36:24 +0800

    Update app.config.ts

--- taro-ts-vue3/src/app.config.ts
+++ taro-ts-vue3/src/app.config.ts
@@ -3,8 +3,34 @@ export default {
     'pages/login/index',
     'pages/select/index',
     'pages/home/index',
-    'pages/detail/index'
+    'pages/detail/index',
+    'pages/add/index',
+    'pages/center/index',
+    'pages/accountDetail/index',
+    'pages/myAlbum/index',
+    'pages/myCollection/index',
+    'pages/myOrder/index'
   ],
+  tabBar: {
+    list: [
+      {
+        pagePath: 'pages/home/index',
+        text: '首页'
+      },
+      {
+        pagePath: 'pages/add/index',
+        text: '添加'
+      },
+      {
+        pagePath: 'pages/center/index',
+        text: '中心'
+      }
+    ],
+    'color': '#000',
+    'selectedColor': '#56abe4',
+    'backgroundColor': '#fff',
+    'borderStyle': 'white'
+  },
   window: {
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#fff',
