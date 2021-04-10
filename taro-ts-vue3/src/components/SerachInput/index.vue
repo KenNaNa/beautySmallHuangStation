f484d6fc193f9b91bceac220b13b4ff5a749c3d8
KenNaNa <m13425477079@163.com>
2021-04-10 10:17:39 +0800

    Update index.vue

---
+++ taro-ts-vue3/src/components/SerachInput/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="search-input-wrap">
+    <van-search
+      :value="value"
+      v-bind="$attrs"
+      placeholder="请输入搜索关键词"
+      :clearable="true"
+      :autofocus="true"
+      input-align="left"
+      @input="input"
+    />
+  </div>
+</template>
+<script lang="ts">
+import { ref, defineComponent, getCurrentInstance } from "vue";
+export default defineComponent({
+  name: "SearchInput",
+  setup() {
+    let value = ref("");
+    let { ctx } = getCurrentInstance();
+    let input = (e: any) => {
+      ctx.$emit("input", e.target.value);
+    };
+    return {
+      value,
+      input,
+    };
+  },
+});
+</script>
+<style lang="scss" scoped>
+</style>
