8bf4a40879b072cd876b5a7a2b8b3a768b57217f
KenNaNa <m13425477079@163.com>
2021-04-10 10:04:14 +0800

    Update index.vue

---
+++ vue3-ts-vite2/src/components/NavBar/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="nav-bar-wrap">
+    <van-nav-bar left-arrow v-bind="$attrs" />
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "NavBar",
+});
+</script>
+<style lang="scss" scoped>
+.nav-bar-wrap {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+}
+</style>
