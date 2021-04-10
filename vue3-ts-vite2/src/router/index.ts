d997f70d6d7e4103cb96d3446b4f0a21875776a1
KenNaNa <m13425477079@163.com>
2021-04-10 10:06:28 +0800

    Update index.ts

--- vue3-ts-vite2/src/router/index.ts
+++ vue3-ts-vite2/src/router/index.ts
@@ -2,6 +2,10 @@ import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 
 const routes: Array<RouteRecordRaw> = [
     {
+        path: '/',
+        redirect: '/login',
+    },
+    {
         path: '/framework',
         name: 'FrameWork',
         redirect: 'home',
