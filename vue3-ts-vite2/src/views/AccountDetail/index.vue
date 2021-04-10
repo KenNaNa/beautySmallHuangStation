e88ba65427504cb9dc2eede0f0b8f5ec3a8040e2
KenNaNa <m13425477079@163.com>
2021-04-10 10:06:53 +0800

    Update index.vue

---
+++ vue3-ts-vite2/src/views/AccountDetail/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="account-detail-wrap">
+    <nav-bar title="账号详情" @click-left="clickLeft"></nav-bar>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import { useRouter } from "vue-router";
+import NavBar from "@/components/NavBar/index.vue";
+export default defineComponent({
+  name: "AccountDetail",
+  components: {
+    "nav-bar": NavBar,
+  },
+  setup(props) {
+    const router = useRouter();
+    const clickLeft = () => {
+      router.go(-1);
+    };
+
+    return {
+      clickLeft,
+    };
+  },
+});
+</script>
+<style lang="">
+</style>
