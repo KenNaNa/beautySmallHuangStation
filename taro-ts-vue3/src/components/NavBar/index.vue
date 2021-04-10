ce584535b5413a718701064197119d1ca8ebef50
KenNaNa <m13425477079@163.com>
2021-04-10 10:17:16 +0800

    Update index.vue

---
+++ taro-ts-vue3/src/components/NavBar/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <view class="nav-bar-wrap">
+    <van-nav-bar left-arrow v-bind="$attrs" />
+  </view>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "NavBar",
+});
+</script>
+<style lang="scss" scoped>
+.nav-bar-wrap {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 10;
+}
+</style>
