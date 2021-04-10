faa4c2c5f1eec18b6b6157bd71d9e32a34cbc617
KenNaNa <m13425477079@163.com>
2021-04-10 10:26:09 +0800

    Update index.vue

--- taro-ts-vue3/src/pages/select/index.vue
+++ taro-ts-vue3/src/pages/select/index.vue
@@ -1,111 +1,44 @@
 <template>
-  <view class="login">
-    <view class="passw-name-box">
-      <view class="name">
-        <AtInput
-          title="昵称"
-          name="userName"
-          v-model:value="state.userName"
-          type="text"
-          placeholder="请输入用户名"
-        />
-      </view>
-      <view class="name">
-        <AtInput
-          name="passWord"
-          title="密码"
-          v-model:value="state.passWord"
-          type="password"
-          placeholder="请输入密码"
-        />
-      </view>
-      <view class="btn">
-        <AtButton type="primary" formType="submit" @click="login"
-          >提交</AtButton
-        >
-      </view>
-    </view>
+  <view class="select-wrap">
+    <van-radio-group
+      v-model="state.checked"
+      direction="horizontal"
+      :icon-size="30"
+      @change="change"
+    >
+      <van-radio name="1">男</van-radio>
+      <van-radio name="2">女</van-radio>
+    </van-radio-group>
   </view>
 </template>
 
-<script>
+<script lang="ts" setup="props">
 import { reactive, getCurrentInstance } from "vue";
-import { useRouter } from "vue-router";
-import { AtInput, AtButton } from "taro-ui-vue3";
-import { toLogin } from "../../api/index";
 import "./index.scss";
+const state = reactive({
+  checked: "",
+});
+const { ctx } = getCurrentInstance();
 
-export default {
-  name: "Login",
-  components: {
-    AtInput,
-    AtButton,
-  },
-  setup() {
-    const router = useRouter();
-    console.log("router", router);
-
-    const { ctx } = getCurrentInstance();
-
-    const state = reactive({
-      userName: "",
-      passWord: "",
-    });
-
-    const initError = () => {
+const change = (e: string) => {
+  const num = parseInt(e);
+  switch (num) {
+    case 1:
+      console.log("男");
       ctx.$toast({
-        type: "fail",
-        message: "登录失败",
+        type: "text",
+        message: "暂无帅哥",
       });
-      window.localStorage.clear();
-      ctx.$taro.navigateTo({
-        url: "pages/login/index",
-      });
-    };
-
-    const login = async () => {
-      if (!state.userName) {
-        ctx.$toast({
-          type: "text",
-          message: "请输入用户名",
+      break;
+    case 2:
+      console.log("女");
+      setTimeout(() => {
+        ctx.$taro.navigateTo({
+          url: "/pages/home/index",
         });
-        return;
-      }
-
-      if (!state.passWord) {
-        ctx.$toast({
-          type: "text",
-          message: "请输入密码",
-        });
-        return;
-      }
-      toLogin({ userName: state.userName, passWord: state.passWord })
-        .then(
-          (res) => {
-            ctx.$toast({
-              type: "success",
-              message: "登录成功",
-            });
-
-            // 设置 token
-            window.localStorage.setItem("accessToken", res.data.token);
-            ctx.$taro.navigateTo({
-              url: "/pages/select/index",
-            });
-          },
-          (error) => {
-            initError();
-          }
-        )
-        .catch((error) => {
-          initError();
-        });
-    };
-
-    return {
-      login,
-      state,
-    };
-  },
+      }, 1000);
+      break;
+    default:
+  }
 };
 </script>
