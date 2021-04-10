b724688271c3e18dbab9ffe549f64b21b27a3b96
KenNaNa <m13425477079@163.com>
2021-04-10 10:12:12 +0800

    Update index.vue

---
+++ vue3-ts-vite2/src/views/MyCollection/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="my-collection-wrap">
+    <nav-bar title="我的收藏" @click-left="clickLeft"></nav-bar>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import { useRouter } from "vue-router";
+import NavBar from "@/components/NavBar/index.vue";
+export default defineComponent({
+  name: "MyCollection",
+  components: {
+    "nav-bar": NavBar,
+  },
+  setup(props) {
+    const router = useRouter();
+    const clickLeft = () => {
+      router.go(-1);
+    };
+
+    return {
+      clickLeft,
+    };
+  },
+});
+</script>
+<style lang="">
+</style>
