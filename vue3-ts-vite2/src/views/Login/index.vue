ff3b56d4be4d4e5f53d71b0d62c8fc693fcd3a6c
KenNaNa <m13425477079@163.com>
2021-04-12 11:23:00 +0800

    Update index.vue

--- vue3-ts-vite2/src/views/Login/index.vue
+++ vue3-ts-vite2/src/views/Login/index.vue
@@ -2,14 +2,14 @@
   <div class="login">
     <van-form @submit="login">
       <van-field
-        v-model="state.userName"
+        v-model="state.username"
         name="用户名"
         label="用户名"
         placeholder="用户名"
         :rules="[{ required: true, message: '请填写用户名' }]"
       />
       <van-field
-        v-model="state.passWord"
+        v-model="state.password"
         type="password"
         name="密码"
         label="密码"
@@ -18,7 +18,7 @@
       />
       <div style="margin: 16px">
         <van-button round block type="primary" native-type="submit">
-          提交
+          登录
         </van-button>
       </div>
     </van-form>
@@ -27,27 +27,33 @@
 <script lang="ts" setup="props">
 import { reactive, getCurrentInstance } from "vue";
 import { useRouter } from "vue-router";
-import { toLogin } from "../../api/index";
+import { toLogin } from "../../api/user";
 const router = useRouter();
 
 const { ctx } = getCurrentInstance();
 
 const state = reactive({
-  userName: "",
-  passWord: "",
+  username: "",
+  password: "",
 });
 
-const initError = () => {
+const initError = (err: any) => {
   ctx.$toast({
     type: "fail",
-    message: "登录失败",
+    message: err.message,
   });
-  window.localStorage.clear();
-  router.push("/login");
 };
 
-const login = async () => {
-  if (!state.userName) {
+const tipFn = (res: any, type: string, cb: Function): void => {
+  ctx.$toast({
+    type,
+    message: res.message,
+  });
+  cb();
+};
+
+const login = () => {
+  if (!state.username) {
     ctx.$toast({
       type: "text",
       message: "请输入用户名",
@@ -55,32 +61,40 @@ const login = async () => {
     return;
   }
 
-  if (!state.passWord) {
+  if (!state.password) {
     ctx.$toast({
       type: "text",
       message: "请输入密码",
     });
     return;
   }
-  toLogin({ userName: state.userName, passWord: state.passWord })
+  toLogin({ username: state.username, password: state.password })
     .then(
-      (res) => {
-        ctx.$toast({
-          type: "success",
-          message: "登录成功",
-        });
-
-        // 设置 token
-        window.localStorage.setItem("accessToken", res.data.token);
+      (res: any) => {
         console.log("res===>", res);
-        router.push("/select");
+        if (res.status === 200) {
+          window.localStorage.setItem("accessToken", res.token);
+          tipFn(res, "success", () => {
+            router.push("/select");
+          });
+        } else if (res.status === 404) {
+          window.localStorage.clear();
+          tipFn(res, "fail", () => {
+            router.push("/register");
+          });
+        } else if (res.status === 400) {
+          window.localStorage.clear();
+          tipFn(res, "fail", () => {
+            router.push("/login");
+          });
+        }
       },
       (error) => {
-        initError();
+        initError(error);
       }
     )
     .catch((error) => {
-      initError();
+      initError(error);
     });
 };
 </script>
