cc39b4421037bf03447fa4d891f39144e9a16f77
KenNaNa <m13425477079@163.com>
2021-04-12 16:50:24 +0800

    Update banner.ts

--- vue3-ts-vite2/src/api/banner.ts
+++ vue3-ts-vite2/src/api/banner.ts
@@ -8,6 +8,11 @@ const getBannerList = (): AxiosPromise => {
     return request.get(url.banners.getBannerList.url)
 };
 
+const getDetailById = (data: Object): AxiosPromise => {
+    return request.post(url.banners.getDetailById.url, data)
+}
+
 export {
-    getBannerList
+    getBannerList,
+    getDetailById
 }
