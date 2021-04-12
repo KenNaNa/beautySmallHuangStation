dc37da468e979c5b75addafe8f5a32913b9e8e94
KenNaNa <m13425477079@163.com>
2021-04-12 15:01:46 +0800

    Update index.vue

--- vue3-ts-vite2/src/components/SerachInput/index.vue
+++ vue3-ts-vite2/src/components/SerachInput/index.vue
@@ -8,25 +8,47 @@
       :autofocus="true"
       input-align="left"
       @input="input"
+      class="search"
     />
+    <van-icon name="play-circle-o" size="20" @click="logout" />
   </div>
 </template>
 <script lang="ts">
 import { ref, defineComponent, getCurrentInstance } from "vue";
+import { useRoute, useRouter } from "vue-router";
 export default defineComponent({
   name: "SearchInput",
   setup() {
     let value = ref("");
+    let route = useRoute();
+    let router = useRouter();
     let { ctx } = getCurrentInstance();
     let input = (e: any) => {
       ctx.$emit("input", e.target.value);
     };
+
+    let logout = () => {
+      window.localStorage.clear();
+      if (route.path !== "/login") {
+        router.push("/login");
+      }
+    };
     return {
       value,
       input,
+      logout,
     };
   },
 });
 </script>
 <style lang="scss" scoped>
+.search-input-wrap {
+  padding: 0 10px 0 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  .search {
+    flex: 2;
+  }
+}
 </style>
