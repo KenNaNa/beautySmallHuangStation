b704960cc8de49eb70ccb35054e90a16efc3a8d7
KenNaNa <m13425477079@163.com>
2021-04-12 16:20:42 +0800

    Create banner.ts

---
+++ vue3-ts-vite2/src/api/banner.ts
@@ -0,0 +1,13 @@
+import type {
+    AxiosPromise
+} from 'axios';
+import request from './request';
+import url from './url';
+
+const getBannerList = (): AxiosPromise => {
+    return request.get(url.banners.getBannerList.url)
+};
+
+export {
+    getBannerList
+}
