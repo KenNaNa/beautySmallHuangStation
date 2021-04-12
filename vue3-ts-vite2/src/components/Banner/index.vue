37ec6317ba679950d4316e8e4191b9cd4067fcfc
KenNaNa <m13425477079@163.com>
2021-04-12 16:18:35 +0800

    Update index.vue

--- vue3-ts-vite2/src/components/Banner/index.vue
+++ vue3-ts-vite2/src/components/Banner/index.vue
@@ -1,8 +1,18 @@
 <template>
   <div class="swipe-content">
-    <van-swipe class="my-swipe" :autoplay="2000" indicator-color="white" lazy-render>
+    <van-swipe
+      class="my-swipe"
+      :autoplay="2000"
+      indicator-color="white"
+      lazy-render
+    >
       <van-swipe-item v-for="item in bannerData" :key="item.id">
-        <img :src="item.images" class="img" :alt="item.title" @click="toDetail(item)" />
+        <img
+          :src="item.images"
+          class="img"
+          :alt="item.title"
+          @click="toDetail(item)"
+        />
       </van-swipe-item>
     </van-swipe>
   </div>
@@ -13,37 +23,44 @@ export default {
   name: "Banner",
   props: {
     bannerData: {
-      type: Array
-    }
+      type: Array,
+    },
   },
   setup(props) {
     const router = useRouter();
-    const toDetail = item => {
+    const toDetail = (item) => {
       router.push({
         name: "Detail",
         params: {
           id: item.id,
-          item: JSON.stringify(item)
-        }
+          item: JSON.stringify(item),
+        },
       });
     };
 
     return {
-      toDetail
+      toDetail,
     };
-  }
+  },
 };
 </script>
 <style lang="scss" scoped>
 .img {
   width: 100%;
-  height: 100%;
+  height: calc(100% - 54px - 50px);
+}
+.van-swipe-item {
+  height: calc(100% - 54px - 50px);
+}
+.swipe-content {
+  padding-top: 54px;
 }
 .my-swipe {
   width: 100%;
 }
 .my-swipe .van-swipe-item {
   width: 100%;
+  height: calc(100vh - 50px);
   //   color: #fff;
   font-size: 1rem;
   //   line-height: 6rem;
