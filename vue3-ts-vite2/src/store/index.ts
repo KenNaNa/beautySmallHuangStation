a456d2e8b99c55e4a942d059179d9a079a4776e7
KenNaNa <m13425477079@163.com>
2021-04-14 18:20:48 +0800

    Update index.ts

--- vue3-ts-vite2/src/store/index.ts
+++ vue3-ts-vite2/src/store/index.ts
@@ -1,21 +1,30 @@
 import { createStore } from 'vuex'
+import MUTATION_TYPS from '@/store/mutations/mutationTypes'
+import ACTION_TYPES from '@/store/actions/actionTypes'
+interface STATE {
+    userinfo?: object
+}
+
+const state: STATE = {
+    userinfo: {}
+}
+
 export default createStore({
-    state: {
-        listData: { 1: 10 },
-        num: 10
-    },
+    state,
     mutations: {
-        setData(state, value) {
-            state.listData = value
+        [MUTATION_TYPS.SET_USERINFO](state, value) {
+            state.userinfo = value
         },
-        addNum(state) {
-            state.num = state.num + 10
+        [MUTATION_TYPS.CLEAR_USERINFO](state) {
+            state.userinfo = {}
         }
     },
     actions: {
-        setData(context, value) {
-            context.commit('setData', value)
+        [ACTION_TYPES.SET_USERINFO](context, value) {
+            context.commit(MUTATION_TYPS.SET_USERINFO, value)
+        },
+        [ACTION_TYPES.CLEAR_USERINFO](context) {
+            context.commit(MUTATION_TYPS.CLEAR_USERINFO)
         }
-    },
-    modules: {}
+    }
 })
