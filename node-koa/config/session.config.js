33ae7d57a2450274f596108fa70479fee74c7907
KenNaNa <m13425477079@163.com>
2021-04-16 08:54:18 +0800

    Update and rename index.txt to session.config.js

---
+++ node-koa/config/session.config.js
@@ -0,0 +1,28 @@
+const sessionConfig = {
+    key: 'koa:sess',
+    /** (string) cookie key (default is koa:sess) cookie 的Name */
+    /** (number || 'session') maxAge in ms (default is 1 days) */
+    /** 'session' will result in a cookie that expires when session/browser is closed */
+    /** Warning: If a session cookie is stolen, this cookie will never expire */
+    maxAge: 86400000,
+    /** cookie 的过期时间 */
+    autoCommit: true,
+    /** (boolean) automatically commit headers (default true) */
+    overwrite: true,
+    /** (boolean) can overwrite or not (default true) */
+    httpOnly: true,
+    /** (boolean) httpOnly or not (default true) */
+    signed: true,
+    /** (boolean) signed or not (default true) */
+    rolling: false,
+    /** (boolean) Force a session identifier cookie to be set on every response. The expiration is reset to the original maxAge, resetting the expiration countdown. (default is false) */
+    renew: false,
+    /** (boolean) renew session when session is nearly expired, so we can always keep user logged in. (default is false)*/
+};
+
+const keys = ["login secret"];
+
+module.exports = {
+    keys,
+    sessionConfig
+};
