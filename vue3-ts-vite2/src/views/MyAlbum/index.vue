f657d67511be7862299c07fe3d195a7273d7ecee
KenNaNa <m13425477079@163.com>
2021-04-16 17:16:45 +0800

    Update index.vue

--- vue3-ts-vite2/src/views/MyAlbum/index.vue
+++ vue3-ts-vite2/src/views/MyAlbum/index.vue
@@ -1,10 +1,17 @@
 <template>
   <div class="my-album-wrap">
     <nav-bar title="我的专辑" @click-left="clickLeft"></nav-bar>
+    <div class="my-album" v-if="albumList.length"></div>
+    <div class="empty" v-else>
+      <van-empty
+        class="custom-image"
+        image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJs-dP9xYTISMvhxJ4KzqobVP2rO_KbHC5ZWJoySgLjbY5-VHsne_5UFdW0y3fzwoNOD8&usqp=CAU"
+      />
+    </div>
   </div>
 </template>
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, reactive, toRefs } from "vue";
 import { useRouter } from "vue-router";
 import NavBar from "@/components/NavBar/index.vue";
 export default defineComponent({
@@ -12,17 +19,27 @@ export default defineComponent({
   components: {
     "nav-bar": NavBar,
   },
-  setup(props) {
+  setup() {
     const router = useRouter();
+    const state = reactive({
+      albumList: [],
+    });
     const clickLeft = () => {
       router.go(-1);
     };
 
     return {
       clickLeft,
+      ...toRefs(state),
     };
   },
 });
 </script>
-<style lang="">
+<style lang="scss" scoped>
+.my-album {
+  margin-top: 50px;
+}
+.empty {
+  margin-top: 50px;
+}
 </style>
