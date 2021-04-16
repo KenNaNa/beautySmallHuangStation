ecd80abf4e7cba17d589a1e10d3af51e4e625504
KenNaNa <m13425477079@163.com>
2021-04-16 17:17:07 +0800

    Update index.vue

--- vue3-ts-vite2/src/views/MyCollection/index.vue
+++ vue3-ts-vite2/src/views/MyCollection/index.vue
@@ -1,10 +1,17 @@
 <template>
   <div class="my-collection-wrap">
     <nav-bar title="我的收藏" @click-left="clickLeft"></nav-bar>
+    <div class="collectList" v-if="collectList.length"></div>
+    <div class="empty" v-else>
+      <van-empty
+        class="custom-image"
+        image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJs-dP9xYTISMvhxJ4KzqobVP2rO_KbHC5ZWJoySgLjbY5-VHsne_5UFdW0y3fzwoNOD8&usqp=CAU"
+      />
+    </div>
   </div>
 </template>
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, reactive, toRefs } from "vue";
 import { useRouter } from "vue-router";
 import NavBar from "@/components/NavBar/index.vue";
 export default defineComponent({
@@ -12,14 +19,18 @@ export default defineComponent({
   components: {
     "nav-bar": NavBar,
   },
-  setup(props) {
+  setup() {
     const router = useRouter();
+    const state = reactive({
+      collectList: [],
+    });
     const clickLeft = () => {
       router.go(-1);
     };
 
     return {
       clickLeft,
+      ...toRefs(state),
     };
   },
 });
