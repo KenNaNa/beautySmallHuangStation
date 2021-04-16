67c356a8b4b7729debd93b8f4a46abe0f9c0ecd9
KenNaNa <m13425477079@163.com>
2021-04-16 17:16:20 +0800

    Update index.vue

--- vue3-ts-vite2/src/views/AccountDetail/index.vue
+++ vue3-ts-vite2/src/views/AccountDetail/index.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="account-detail-wrap">
     <nav-bar title="账号详情" @click-left="clickLeft"></nav-bar>
+    <div class="account-detail">
+      <img src="../../assets/img.jpg" width="80" height="80" />
+      <span class="account">{{ account }}</span>
+    </div>
   </div>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
 import { useRouter } from "vue-router";
 import NavBar from "@/components/NavBar/index.vue";
+import { getLocalStorage } from "@/utils/localStorage";
 export default defineComponent({
   name: "AccountDetail",
   components: {
     "nav-bar": NavBar,
   },
-  setup(props) {
+  setup() {
+    const account = JSON.parse(getLocalStorage("userInfo")).username;
     const router = useRouter();
     const clickLeft = () => {
       router.go(-1);
@@ -20,9 +26,19 @@ export default defineComponent({
 
     return {
       clickLeft,
+      account,
     };
   },
 });
 </script>
-<style lang="">
+<style lang="scss" scoped>
+.account-detail {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 50px;
+  height: calc(100vh - 50px);
+  overflow: hidden;
+}
 </style>
