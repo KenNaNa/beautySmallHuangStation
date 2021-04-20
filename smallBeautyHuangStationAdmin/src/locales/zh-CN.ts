6a8ab0f7590dd308dd0c2c7051aba0b18d30f7dc
KenNaNa <m13425477079@163.com>
2021-04-20 08:49:27 +0800

    Update zh-CN.ts

--- smallBeautyHuangStationAdmin/src/locales/zh-CN.ts
+++ smallBeautyHuangStationAdmin/src/locales/zh-CN.ts
@@ -1,3 +1,35 @@
 export default {
-  Message: '这是一条消息。',
+  CHINESE: "简体中文",
+  ENGLISH: "English",
+  ApiError: {
+    E_00001: '用户不存在~',
+    E_00002: '密码错误~',
+  },
+  FormValidate: {
+    Required: "{field}是必填的"
+  },
+  Components: {
+    PersonalCenter: '个人中心',
+    PersonalSetting: '个人设置',
+    Logout: '退出登录',
+  },
+  Login: {
+    Title: "美女小黄站 - 登录",
+    Description: '欢迎来到 美女小黄站，请登录',
+    Username: '用户名',
+    Password: '密码',
+    Login: '登录',
+    Error: "用户名或密码错误~",
+    LoginSuccess: '登录成功',
+    WelcomeToLogin: '您好，欢迎使用 美女小黄站 ！',
+  },
+  ErrorPage: {
+    403: '抱歉，您无权访问此页面。',
+    404: '抱歉，您访问的页面不存在。',
+    BackHome: '回到首页',
+    ReturnPrevious: '返回上一页',
+  },
+  Overview: {
+    Title: '总览 - Ant Design Admin',
+  },
 };
