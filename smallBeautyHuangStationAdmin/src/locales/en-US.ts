3fb32b9153fafcbfb782ddb1f4fcf8649612ed38
KenNaNa <m13425477079@163.com>
2021-04-20 08:49:12 +0800

    Update en-US.ts

--- smallBeautyHuangStationAdmin/src/locales/en-US.ts
+++ smallBeautyHuangStationAdmin/src/locales/en-US.ts
@@ -1,3 +1,35 @@
 export default {
-  Message: 'This is a message.',
+  Chinese: "简体中文",
+  English: "English",
+  ApiError: {
+    E_00001: 'User does not exist~',
+    E_00002: 'Incorrect password~',
+  },
+  Components: {
+    PersonalCenter: 'Personal Center',
+    PersonalSetting: 'Personal Setting',
+    Logout: 'Logout',
+  },
+  FormValidate: {
+    Required: "{field} is required"
+  },
+  Login: {
+    Title: "Samll Beauty Huang Station Admin - Login",
+    Description: 'Welcome to Samll Beauty Huang Station Admin, Please login',
+    Username: 'Username',
+    Password: 'Password',
+    Login: 'Login',
+    Error: "Username or password is wrong~",
+    LoginSuccess: "Login Success",
+    WelcomeToLogin: 'Hello, welcome to Small Beauty Huang Station Backend!'
+  },
+  ErrorPage: {
+    403: 'Sorry, you are not authorized to access this page.',
+    404: 'Sorry, the page you visited does not exist.',
+    BackHome: 'Back Home',
+    ReturnPrevious: 'Go Back',
+  },
+  Overview: {
+    Title: 'Overview - Ant Design Admin',
+  },
 };
