49f34762307c50a1c2b0fd05003a05e1939d11e1
KenNaNa <m13425477079@163.com>
2021-04-20 08:48:22 +0800

    Update utils.ts

--- smallBeautyHuangStationAdmin/src/plugins/utils.ts
+++ smallBeautyHuangStationAdmin/src/plugins/utils.ts
@@ -1,2 +1,21 @@
-export default ({ app }: any, inject: any) => {
+type RouterType = 'push' | 'replace' | 'external';
+import Cookies from 'js-cookie';
+import { userToken } from '@/config/constants.ts';
+
+export const logout = () => {
+    Cookies.remove(userToken);
+    window.location.reload();
+};
+export default ({ router }: any, inject: any) => {
+    inject("navigateTo", (url: string | undefined, target: RouterType = 'push') => {
+        if (!url) {
+            router.back();
+        } else {
+            if (target === 'external') {
+                window.open(url);
+            } else {
+                router[target](url);
+            }
+        }
+    });
 };
