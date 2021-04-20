d6e41d3bd699742bb55a15473266403a8ae75c36
KenNaNa <m13425477079@163.com>
2021-04-20 08:43:01 +0800

    Update shims.d.ts

--- smallBeautyHuangStationAdmin/shims.d.ts
+++ smallBeautyHuangStationAdmin/shims.d.ts
@@ -1,7 +1,26 @@
+import { ComponentCustomProperties } from 'vue'
+import { Store } from 'vuex'
 declare module '*.vue' {
   import { defineComponent } from 'vue';
   const Component: ReturnType<typeof defineComponent>;
   export default Component;
 }
+declare module '*.tsx';
+declare module '*.ts';
+declare module '@vue/runtime-core' {
+  // provide typings for `this.$store`
+  interface ComponentCustomProperties {
+    $store: Store<State>
+  }
 
-declare const $t: (string) => string;
+  export interface MutationTree<S> {
+    [key: string]: Mutation<S>;
+  }
+}
+
+declare module '@convue-lib/phone';
+declare module '@convue-lib/otp-input';
+declare module '@convue-lib/otp-send';
+declare module '@convue-lib/formatted';
+declare module '@convue-lib/scroll';
+declare module '@convue-lib/utils';
