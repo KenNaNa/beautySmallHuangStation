cd89a1915f4fc2ceab7b8a1c748403bccb83cce3
KenNaNa <m13425477079@163.com>
2021-04-20 08:50:16 +0800

    Update default.tsx

--- smallBeautyHuangStationAdmin/src/layouts/default.tsx
+++ smallBeautyHuangStationAdmin/src/layouts/default.tsx
@@ -1,12 +1,49 @@
-import { defineComponent } from 'vue';
-
+import { defineComponent, ref } from 'vue';
+import { useToggle } from '@ant-design-vue/use';
+import { queryMedia } from '@convue-lib/utils';
+import './default.less';
 export default defineComponent({
-  setup() {},
-  render() {
-    return (
-      <div>
-        <router-view></router-view>
-      </div>
+  setup() {
+    const [collapsed, { toggle: toggleSider }] = useToggle(false);
+    const [visible, { toggle: toggleDrawer }] = useToggle(false);
+    const isMobile = ref<boolean>(false);
+    queryMedia((data: string) => {
+      isMobile.value = data === 'xs';
+    });
+    const toggle = () => {
+      isMobile.value ? toggleDrawer() : toggleSider();
+    };
+    return () => (
+      <a-layout id="layout">
+        {isMobile.value ? (
+          <a-drawer
+            class="layout-drawer"
+            placement="left"
+            closable={false}
+            v-model={[visible.value, 'visible']}
+          >
+            <layout-asider collapsed={false}></layout-asider>
+          </a-drawer>
+        ) : (
+          <a-layout-sider
+            class="layout-asider"
+            v-model={[collapsed.value, 'collapsed']}
+            breakpoint="md"
+            width={250}
+            trigger={null}
+            collapsible
+          >
+            <layout-asider collapsed={collapsed.value}></layout-asider>
+          </a-layout-sider>
+        )}
+
+        <a-layout>
+          <layout-header collapsed={collapsed.value} onToggle={toggle}></layout-header>
+          <a-layout-content class="layout-content">
+            <router-view></router-view>
+          </a-layout-content>
+        </a-layout>
+      </a-layout>
     );
   },
 });
