74d1f88b82d997a86fb0a80c9684779558472814
KenNaNa <m13425477079@163.com>
2021-04-22 09:11:40 +0800

    Create 403.tsx

---
+++ smallBeautyHuangStationAdmin/src/pages/403.tsx
@@ -0,0 +1,38 @@
+{
+    /* <route>
+    meta:
+      layout: empty
+      head:
+        title: 美女小黄站 - 403
+  </route> */
+}
+
+import { defineComponent, getCurrentInstance } from 'vue';
+import { useI18n } from 'vue-i18n';
+
+export default defineComponent({
+    setup() {
+        const { t } = useI18n();
+        const navigateTo = getCurrentInstance()?.appContext.config.globalProperties.$navigateTo;
+
+        return () => (
+            <div class="pt-12">
+                <a-result
+                    status="403"
+                    title="403"
+                    sub-title={t('ErrorPage.403')}
+                    v-slots={{
+                        extra: () => (
+                            <a-space>
+                                <a-button onClick={() => navigateTo('/')}>{t('ErrorPage.BackHome')}</a-button>
+                                <a-button type="primary" onClick={() => navigateTo()}>
+                                    {t('ErrorPage.ReturnPrevious')}
+                                </a-button>
+                            </a-space>
+                        ),
+                    }}
+                ></a-result>
+            </div>
+        );
+    },
+});
