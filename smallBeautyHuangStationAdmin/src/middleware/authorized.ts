fdf03db74a9b5de4a7ea94dc3e3a219f7d2b3d2a
KenNaNa <m13425477079@163.com>
2021-04-22 09:04:09 +0800

    Create authorized.ts

---
+++ smallBeautyHuangStationAdmin/src/middleware/authorized.ts
@@ -0,0 +1,32 @@
+import Cookies from 'js-cookie';
+import { ActionTypes } from '../store/index';
+import { userToken, whiteList } from './../config/constants';
+import { logout } from './../plugins/utils';
+
+export default async ({ route, store, redirect, i18n }: any) => {
+    const userId = Cookies.get(userToken);
+    if (userId) {
+        if (route.path === '/login') {
+            redirect('/');
+        } else {
+            if (!store.state.userInfo.id) {
+                try {
+                    await store.dispatch(ActionTypes.QUERY, userId);
+                } catch (err) {
+                    logout();
+                }
+            }
+            if (!store.state.menu.length) {
+                try {
+                    await store.dispatch(ActionTypes.MENU, i18n.global.locale);
+                } catch (err) {
+                    console.log(err);
+                }
+            }
+        }
+    } else {
+        if (!whiteList.includes(route.path)) {
+            redirect('/login');
+        }
+    }
+};
