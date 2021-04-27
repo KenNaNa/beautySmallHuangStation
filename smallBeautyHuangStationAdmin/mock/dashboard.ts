18a7cc1cd56cf7f5c1b16ff400e34a55a10c6b58
KenNaNa <m13425477079@163.com>
2021-04-27 08:54:32 +0800

    Create dashboard.ts

---
+++ smallBeautyHuangStationAdmin/mock/dashboard.ts
@@ -0,0 +1,81 @@
+import { MockMethod } from 'vite-plugin-mock';
+import {
+    miniArea,
+    miniBar,
+    barData,
+    barData2,
+    rankList,
+    searchData,
+    pieData,
+    projects,
+    movements,
+    radarData,
+    teams,
+} from '../schemes/dashboard';
+
+export default [
+    {
+        url: '/api/dashboard/overview',
+        method: 'get',
+        response: () => {
+            return {
+                code: 'S_0000',
+                data: {
+                    miniArea,
+                    miniBar,
+                    barData,
+                    barData2,
+                    rankList,
+                    searchData,
+                    pieData,
+                },
+            };
+        },
+    },
+
+    {
+        url: '/api/workplace/projects',
+        method: 'get',
+        response: () => {
+            return {
+                code: 'S_0000',
+                data: projects,
+            };
+        },
+    },
+
+    {
+        url: '/api/workplace/movements',
+        method: 'get',
+        response: () => {
+            return {
+                code: 'S_0000',
+                data: movements,
+            };
+        },
+    },
+
+    {
+        url: '/api/workplace/radar',
+        method: 'get',
+        response: () => {
+            return {
+                code: 'S_0000',
+                data: radarData,
+            };
+        },
+    },
+
+    {
+        url: '/api/workplace/teams',
+        method: 'get',
+        response: () => {
+            return {
+                code: 'S_0000',
+                data: teams,
+            };
+        },
+    },
+
+
+] as MockMethod[];
