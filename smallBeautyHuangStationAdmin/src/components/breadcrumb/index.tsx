ae27c15ffe2995d0d5e8ab19048e977c56fe9bdd
KenNaNa <m13425477079@163.com>
2021-04-27 08:57:50 +0800

    Create index.tsx

---
+++ smallBeautyHuangStationAdmin/src/components/breadcrumb/index.tsx
@@ -0,0 +1,35 @@
+import { computed, defineComponent, toRaw } from 'vue';
+import { useRoute } from 'vue-router';
+import { useI18n } from 'vue-i18n';
+export default defineComponent({
+    setup() {
+        const route = useRoute();
+        const { t } = useI18n();
+        console.log("route====>", route)
+        const matched = computed(() => {
+            const currentMatched = toRaw(route.matched)
+                .slice(1)
+                .filter((n) => n.meta.title);
+
+            if (currentMatched.find((item) => item.path === '/')) {
+                return currentMatched;
+            } else {
+                return [{ path: '/', meta: { title: 't("Home.Title")' } }, ...currentMatched];
+            }
+        });
+        console.log("matched", matched)
+        return () => (
+            <a-breadcrumb class="d-none d-sm-inline-block">
+                {matched.value.map((match) => (
+                    <a-breadcrumb-item>
+                        <router-link to={match.path}>
+                            {/^t\(.+\)$/.test(match.meta.title)
+                                ? t(match.meta.title.slice(3, -2))
+                                : match.meta.title}
+                        </router-link>
+                    </a-breadcrumb-item>
+                ))}
+            </a-breadcrumb>
+        );
+    },
+});
